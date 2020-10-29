e97195aa53b51e57a044f2c041a6d4b0bebeb325
Ravi Gulia <iamravigulia@users.noreply.github.com>
2020-10-29 14:55:33 +0530

    Update FillupController.php

--- src/Controllers/FillupController.php
+++ src/Controllers/FillupController.php
@@ -2,6 +2,8 @@
 
 namespace edgewizz\fillup\Controllers;
 use App\Http\Controllers\Controller;
+use Edgewizz\Fillup\Models\FillupAns;
+use Edgewizz\Fillup\Models\FillupQues;
 use Illuminate\Http\Request;
 
 class FillupController extends Controller
@@ -9,5 +11,19 @@ class FillupController extends Controller
     //
     public function test(){
         dd('hello');
+    }
+    public function store(Request $request){
+        // dd('got here');
+        $fillupQues = new FillupQues();
+        $fillupQues->question = $request->question;
+        $fillupQues->save();
+        $fillupAns1 = new FillupAns();
+        $fillupAns1->question_id = $fillupQues->id;
+        $fillupAns1->answer = $request->answer;
+        $fillupAns1->save();
+        return back();
+    }
+    public function edit($id, Request $request){
+        
     }
 }
